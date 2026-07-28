04b1c001f66cb718cd657b4f878bbec92a664aaf
Jeremiah-Jefry <g.jeremiahjefry06@gmail.com>
2026-07-28 09:25:48 +0530

    feat: add multyTable function to support custom multiplication table range

--- Multiplication Table.py
+++ Multiplication Table.py
@@ -4,3 +4,12 @@ def Multiply(n):
 
 n = int(input("Enter a number: "))
 Multiply(n)
+
+
+def multyTable(n,x):
+    for i in range(1,x+1):
+        print(f"{n} * {i} = {n*i}")
+
+n = int(input("Enter a number: "))
+x = int(input("Enter a range: "))
+multyTable(n,x)
