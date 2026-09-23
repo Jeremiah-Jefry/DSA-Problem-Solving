e16cac01f1007eb4107081eb06aed06db80cfe88
Jeremiah Jefry G <g.jeremiahjefry06@gmail.com>
2026-09-23 10:34:45 +0530

    Time: 1 ms (88.65%), Space: 20.6 MB (25.91%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,5 +4,5 @@ class Solution:
 
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[j], nums[i] = nums[i], nums[j]
-                j += 1
+                nums[j],nums[i] = nums[i],nums[j] 
+                j+=1
